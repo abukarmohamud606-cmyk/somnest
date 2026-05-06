8125d8786a4c46c9a8637fc5396f537301802e1e
somnest.github.io/somNest-Muqdisho <abukarmohamud606@gmail.com>
2026-05-06 21:21:37 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'somnest-v1';
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request).catch(function() {
+    return caches.match(e.request);
+  }));
+});
